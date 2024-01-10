6334db0cec30705c3cd3913c094ff59eecdf1f8f
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-01-10 18:04:30 +0530

    fix: class based components continued

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -7,16 +7,23 @@ export default class UserClass extends React.Component {
 
     this.state = {
       count: 0,
-      count2: 1,
     };
   }
 
   render() {
-    const { count, count2 } = this.state;
+    const { count } = this.state;
     return (
       <div className="user">
         <h3>Count: {count}</h3>
-        <h3>Count2: {count2}</h3>
+        <button
+          onClick={() => {
+            this.setState({
+              count: this.state.count + 1,
+            });
+          }}
+        >
+          Increase Count
+        </button>
         <h1>Name: {this.props.name}</h1>
         <h2>Location: {this.props.location}</h2>
         <h3>Age: 25</h3>
