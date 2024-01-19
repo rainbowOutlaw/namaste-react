e34a8690bf16fabf6c775ad00b9814b3c1548158
rainbowOutlaw <148523561+rainbowOutlaw@users.noreply.github.com>
2024-01-19 20:25:02 +0530

    feat: add items list to cart page

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,14 @@
+import { useSelector } from "react-redux";
 import ItemsList from "./ItemsList";
 
 export default function Cart() {
+  const cartItems = useSelector((store) => store.cart.items);
   return (
     <div className="text-center m-4 p-4">
       <h1 className="font-bold text-2xl">Cart</h1>
+      <div className="w-6/12 mx-auto">
+        <ItemsList items={cartItems} />
+      </div>
     </div>
   );
 }
